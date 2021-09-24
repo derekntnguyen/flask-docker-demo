9b212894525034ea4c495f504224539b04f503b6
Derek Nguyen <dnguyen0305@gmail.com>
2021-09-24 21:49:46 +0000

    added response schema and built out template

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 Test application to experiment with Flask APIs. These APIs
 will be used to serve inferences to predictive models
 """
+import pickle
 
 from flask import Flask, jsonify, request, make_response, abort
 from dataclasses import dataclass
@@ -15,12 +16,44 @@ import pandas as pd
 app = Flask(__name__)
 
 
+class Model:
+    """Object to store model while running"""
+
+    model = None
+
+    @classmethod
+    def get_model(cls):
+        with open("/workspaces/flask-docker-demo/model.obj", "rb") as f:
+            cls.model = pickle.load(f)
+
+    @classmethod
+    def predict(cls, data):
+        if cls.model == None:
+            # cls.get_model()
+            pass
+
+        data["probability"] = 0.9
+        data["label"] = "Accept"
+        #  data["probability"] = cls.model.predict(data)
+        return data
+
+
 @dataclass
-class Scorecard:
+class ScorecardRequest:
+    """Request dataclass to validate API inputs"""
+
     name: str = dataclasses.field(
         metadata=desert.metadata(
             fields.Str(
                 required=True, validate=validate.Length(min=1, error="Name field cannot be empty")
+            )
+        )
+    )
+    id: str = dataclasses.field(
+        metadata=desert.metadata(
+            fields.Str(
+                required=True,
+                validate=validate.Length(min=0, error="ID field cannot be empty"),
             )
         )
     )
@@ -44,7 +77,45 @@ class Scorecard:
         """
         Test
         """
-        self.age = self.age + 1
+        pass
+
+
+@dataclass
+class ScorecardResponse:
+    """Response dataclass to validate API inputs"""
+
+    id: str = dataclasses.field(
+        metadata=desert.metadata(
+            fields.Str(
+                required=True,
+                validate=validate.Length(min=1, error="ID field cannot be empty"),
+            )
+        )
+    )
+    probability: float = dataclasses.field(
+        metadata=desert.metadata(
+            fields.Float(
+                required=True,
+                validate=validate.Range(min=0, max=1, error="Probability field must be [0,1]"),
+            )
+        )
+    )
+    label: str = dataclasses.field(
+        metadata=desert.metadata(
+            fields.Str(
+                required=True,
+                validate=validate.OneOf(
+                    choices=["Accept", "Fail"], error="label field must either be Accept or Fail"
+                ),
+            )
+        )
+    )
+
+    def __post_init__(self):
+        """
+        Test
+        """
+        pass
 
 
 @app.route("/ping", methods=["GET"])
@@ -55,25 +126,26 @@ def ping():
 
 @app.route("/predict", methods=["GET"])
 def predict():
-    data = request.get_json()
-    validated_response = desert.schema(Scorecard, meta={"unknown": EXCLUDE})
+    validated_request = desert.schema(ScorecardRequest, meta={"unknown": EXCLUDE})
+    validated_response = desert.schema(ScorecardResponse, meta={"unknown": EXCLUDE})
+
+    request_data = request.get_json()
 
     try:
-        policy = validated_response.load(data)
+        policy = validated_request.load(request_data)
     except ValidationError as err:
         abort(jsonify(err.messages))
 
-    return jsonify(policy.__dict__)
+    data = pd.DataFrame(policy.__dict__, index=[0])
 
-    # if not data:
-    #     abort("Invalid data")
+    data = Model.predict(data)
 
-    # df = pd.DataFrame(data["data"])
-    # df["score"] = 0.8
-    # predictions = df[["id", "score"]]
-    # response = predictions.to_json(orient="split")
+    try:
+        scored_policy = validated_response.load(data.to_dict(orient="records")[0])
+    except ValidationError as err:
+        abort(jsonify(err.messages))
 
-    # return make_response(response, 200)
+    return jsonify(scored_policy.__dict__)
 
 
 if __name__ == "__main__":
